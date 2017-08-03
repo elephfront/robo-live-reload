76d402addf80742d9cde39edc0f6790590453213
Yves P <havokinspiration@gmail.com>
2017-08-03 14:07:32 +0200

    Prepare the Robo task specific classes

---
+++ src/Task/LiveReload.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Copyright (c) Yves Piquel (http://www.havokinspiration.fr)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Yves Piquel (http://www.havokinspiration.fr)
+ * @link          http://github.com/elephfront/robo-live-reload
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+declare(strict_types=1);
+namespace Elephfront\RoboLiveReload\Task;
+
+use Robo\Contract\TaskInterface;
+use Robo\Task\BaseTask;
+
+class LiveReload extends BaseTask implements TaskInterface
+{
+
+    /**
+     * @return \Robo\Result
+     */
+    public function run()
+    {
+        // TODO: Implement run() method.
+    }
+}
---
+++ src/Task/Loader/LoadLiveReloadTaskTrait.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Copyright (c) Yves Piquel (http://www.havokinspiration.fr)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Yves Piquel (http://www.havokinspiration.fr)
+ * @link          http://github.com/elephfront/robo-live-reload
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+declare(strict_types=1);
+namespace Elephfront\RoboLiveReload\Task\Loader;
+
+use Elephfront\RoboLiveReload\Task\LiveReload;
+
+trait LoadLiveReloadTaskTrait
+{
+    
+    /**
+     * Exposes the LiveReload task.
+     *
+     * @return \Elephfront\RoboLiveReload\Task\LiveReload Instance of the Sass Task
+     */
+    protected function taskLiveReload()
+    {
+        return $this->task(LiveReload::class);
+    }
+}
