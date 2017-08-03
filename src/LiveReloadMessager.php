02541a55e35bd175e11300195c004e685e77123c
Yves P <havokinspiration@gmail.com>
2017-08-03 22:11:55 +0200

    Add the LiveReloadMessager class

---
+++ src/LiveReloadMessager.php
@@ -0,0 +1,98 @@
+<?php
+namespace LiveReload;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+/**
+ * Class LiveReloadMessager
+ *
+ * In charge of storing connections to the Ratchet server and dispatching messages to them when a message is sent to
+ * the server.
+ */
+class LiveReloadMessager implements MessageComponentInterface
+{
+
+    /**
+     * Object storage where the connection to the WebSocket server are stored
+     *
+     * @var \SplObjectStorage
+     */
+    protected $clients;
+
+    /**
+     * Constructor.
+     */
+    public function __construct()
+    {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    /**
+     * Called when a connection is opened to the WebSocket server.
+     * It will store the connection to the `clients` property.
+     *
+     * @param \Ratchet\ConnectionInterface $connection
+     * @return void
+     */
+    public function onOpen(ConnectionInterface $connection)
+    {
+        $this->clients->attach($connection);
+
+        echo sprintf('New connection ! (%s)\n', $connection->resourceId);
+    }
+
+    /**
+     * Called when a connection is sending a message to the WebSocket server.
+     * It will store the connection to the `clients` property.
+     *
+     * @param \Ratchet\ConnectionInterface $connection Connection sending the $message.
+     * @param string $message Message being sent.
+     * @return void
+     */
+    public function onMessage(ConnectionInterface $connection, $message)
+    {
+        $targetCount = count($this->clients) - 1;
+        echo sprintf(
+            'Connection `%d` sending message "%s" to %d other connection%s' . "\n",
+            $connection->resourceId,
+            $message,
+            $targetCount,
+            $targetCount == 1 ? '' : 's'
+        );
+
+        foreach ($this->clients as $client) {
+            if ($connection !== $client) {
+                $client->send($message);
+            }
+        }
+    }
+
+    /**
+     * Called when a connection is closing from the WebSocket server.
+     * It will remove the connection from the `clients` property.
+     *
+     * @param \Ratchet\ConnectionInterface $connection Connection sending the $message.
+     * @return void
+     */
+    public function onClose(ConnectionInterface $connection)
+    {
+        $this->clients->detach($connection);
+
+        echo sprintf('Connection `%s` has disconnected\n', $connection->resourceId);
+    }
+
+    /**
+     * Called when a connection encounters an error.
+     * It will remove the connection from the `clients` property.
+     *
+     * @param \Ratchet\ConnectionInterface $connection Connection sending the $message.
+     * @param \Exception $exception The exception thrown
+     * @return void
+     */
+    public function onError(ConnectionInterface $connection, \Exception $exception) {
+        echo sprintf('An error has occurred: `%s`\n', $exception->getMessage());
+
+        $connection->close();
+    }
+}
